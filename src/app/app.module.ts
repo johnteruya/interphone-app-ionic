93d27d1a48f27c102b1be4e11e4c48dee8d953f1
johnteruya <jteruya9@gmail.com>
2019-10-11 21:29:57 -0300

    Criando pagina de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
+import { CartService } from '../services/domain/cart.service';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -38,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     StorageService,
     ClienteService,
     AuthInterceptorProvider,
-    ProdutoService
+    ProdutoService,
+    CartService
   ]
 })
 export class AppModule {}
